7eacd4cc997e96f24cc5ca9bad8dfc7308771a6e
Bruno Daniel <danibrunoo18@gmail.com>
2024-07-30 16:59:20 -0300

    fix

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const indx = express();
+
+const app = express();
 const porta = 8800;
 const autor_router = require('./routes/autor.js'); 
 const livro_router = require('./routes/livro.js'); 
 const usuario_router = require('./routes/usuario.js'); 
-indx.use(express.json());
-indx.use("/autor", autor_router);
-indx.use("/livro", livro_router);
-indx.use("/usuario", usuario_router);
-indx.listen(porta, ()=> {
-    console.log("Link do Servidor: "+porta);
+
+app.use(express.json());
+app.use
+
+app.use("/autor", autor_router);
+app.use("/livro", livro_router);
+app.use("/usuario", usuario_router);
+app.listen(porta, ()=> {
+    console.log("Link do Servidor: http://localhost:"+porta);
 })
 
 
--- api/routes/autor.js
+++ api/routes/autor.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const rota = express.Router();
 const autor_controller = require("../controllers/autor.js");
+
 rota.get("/", autor_controller.listar_autores_controller);
 rota.post("/", autor_controller.cadastrar_autor_controller);
 rota.post("/deletar/:id", autor_controller.deletar_autor_controller);
